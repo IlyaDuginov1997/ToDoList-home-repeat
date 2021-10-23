e23b36953f9de1fe024573190fd8ff020e339667
Ilya <IlyaDuginov1997@gmail.com>
2021-10-23 14:12:24 +0300

    Updates

---
+++ precache-manifest.a8e4334dd3da398f1311c5526eb87677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0969712f490116204dc371bd2a66c365",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "1b19f11aaf2333846428",
+    "url": "/Todolist/static/css/main.f433161e.chunk.css"
+  },
+  {
+    "revision": "330ff0ae4853f8923164",
+    "url": "/Todolist/static/js/2.265dc205.chunk.js"
+  },
+  {
+    "revision": "eb314788fe3fb32ce23a1d6fa9676554",
+    "url": "/Todolist/static/js/2.265dc205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b19f11aaf2333846428",
+    "url": "/Todolist/static/js/main.5c3d9bbd.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
