dd89c7cfc5bc18054d288c23912fb8e71fbe45cd
Ilya <IlyaDuginov1997@gmail.com>
2021-11-09 23:45:39 +0300

    Lesson-17 (part 1. Redux-toolkit, install, configureStore)

--- src/Redux-store/Store.ts
+++ src/Redux-store/Store.ts
@@ -4,16 +4,25 @@ import {todolistReducer} from './TodolistReducer/todolist-reducer';
 import {appReducer} from './AppReducer/app-reducer';
 import thunkMiddleware from 'redux-thunk';
 import {authReducer} from './AuthReducer/auth-reducer';
+import {configureStore} from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
     tasks: taskReducer,
     todolists: todolistReducer,
     app: appReducer,
     auth: authReducer,
-})
+});
 
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+// native Redux
+// export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+
+export const store = configureStore(
+    {
+        reducer: rootReducer,
+        middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+    }
+);
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 // @ts-ignore
-window.store = store.getState()
+window.store = store.getState();
